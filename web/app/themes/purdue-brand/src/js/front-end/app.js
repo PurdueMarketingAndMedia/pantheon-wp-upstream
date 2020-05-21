816bad28abbcdf74db9f2e07523d2baa973b64ef
Chris Carrel <cjcarrel@purdue.edu>
2020-05-21 11:49:32 -0400

    Reduced Plugins and Added Official Brand Theme

--- web/app/themes/purdue-brand/src/js/front-end/app.js
+++
@@ -1,5 +0,0 @@
-import "./navigation.js";
-import "./accordion.js";
-import "./skip-link-focus-fix.js";
-import "./hideAjaxLoadMoreButton";
-import "./google-graph-spacing.js";
--- web/app/themes/purdue-brand/src/js/front-end/hideAjaxLoadMoreButton.js
+++
@@ -1,6 +0,0 @@
-window.almDone = function(){
-	[...document.querySelectorAll('.alm-btn-wrap')].forEach((el)=>{
-        el.style.display = "none";
-    });
-};
-window.almDone();
